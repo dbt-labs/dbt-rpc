eaef7e77b8f037a97f3ff96f88a153c862ffb59d
Kyle Wigley <kyle@dbtlabs.com>
2021-10-06 12:53:57 -0400

    update impl project commands

--- dbt_rpc/contracts/rpc.py
+++ dbt_rpc/contracts/rpc.py
@@ -120,6 +120,7 @@ class RPCDocsGenerateParameters(RPCParameters):
 
 @dataclass
 class RPCBuildParameters(RPCParameters):
+    resource_types: Optional[List[str]] = None
     threads: Optional[int] = None
     models: Union[None, str, List[str]] = None
     select: Union[None, str, List[str]] = None
