324a3c019de94c1b11fde0baaa1e3d75f5133372
Chenyu Li <chenyu.li@dbtlabs.com>
2022-09-20 12:36:49 -0700

    fix dev-requirements.txt (#95)
    * Update dev-requirements.txt
    
    * adjust for new update in core
    
    * add help text
    
    * fix flake8

--- dbt_rpc/__main__.py
+++ dbt_rpc/__main__.py
@@ -13,7 +13,7 @@ from dbt.adapters.factory import reset_adapters, cleanup_connections
 import dbt.tracking
 
 from dbt.utils import ExitCodes
-from dbt.config.profile import DEFAULT_PROFILES_DIR, read_user_config
+from dbt.config.profile import read_user_config
 from dbt.exceptions import (
     RuntimeException,
     InternalException,
@@ -272,8 +272,9 @@ def _build_base_subparser():
         dest='sub_profiles_dir',  # Main cli arg precedes subcommand
         type=str,
         help='''
-        Which directory to look in for the profiles.yml file. Default = {}
-        '''.format(DEFAULT_PROFILES_DIR)
+        Which directory to look in for the profiles.yml file.
+        If not set, dbt will look in the current working directory first, then HOME/.dbt/
+        '''
     )
 
     base_subparser.add_argument(
@@ -483,8 +484,9 @@ def parse_args(args, cls=DBTArgumentParser):
         dest='profiles_dir',
         type=str,
         help='''
-        Which directory to look in for the profiles.yml file. Default = {}
-        '''.format(DEFAULT_PROFILES_DIR)
+        Which directory to look in for the profiles.yml file.
+        If not set, dbt will look in the current working directory first, then HOME/.dbt/
+        '''
     )
 
     p.add_argument(
