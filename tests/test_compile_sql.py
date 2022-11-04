a0f8438d6276b91e7f1c04b7bd164cebb7a77284
Chenyu Li <chenyu.li@dbtlabs.com>
2022-11-04 09:57:21 -0700

    add test for compile macro

--- tests/test_compile_sql.py
+++ tests/test_compile_sql.py
@@ -23,3 +23,22 @@ def test_rpc_run_sql_nohang(
     with querier_ctx as querier:
         result = querier.async_wait_for_result(querier.compile_sql('select 1 as id', language='python'))
         assert 'def ref(*args,dbt_load_df_function):' in result['results'][0]['compiled_sql']
+
+@pytest.mark.supported('any')
+def test_rpc_compile_macro(
+    project_root, profiles_root, dbt_profile, unique_schema
+):
+    project = ProjectDefinition(
+        models={'my_model.sql': 'select 1 as id'}
+    )
+    querier_ctx = get_querier(
+        project_def=project,
+        project_dir=project_root,
+        profiles_dir=profiles_root,
+        schema=unique_schema,
+    )
+    with querier_ctx as querier:
+        result = querier.async_wait_for_result(querier.compile_sql('{% macro test()%}\n 1\n{%endmacro%}\n {{ test() }}', ))
+        breakpoint()
+        assert '\n \n 1\n' == result['results'][0]['compiled_sql']
+
