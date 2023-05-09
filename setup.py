0968c8fb81b896f8e87475e12e143b2d00398b1a
Chenyu Li <chenyu.li@dbtlabs.com>
2023-05-09 11:58:44 -0700

    Cl/fix poll list result main (#138)
    * fix poll list task
    
    * fix dbt exception
    
    * nits
    
    * bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 
 package_name = "dbt-rpc"
-package_version = "0.4.0"
+package_version = "0.4.1"
 description = """ A JSON RPC server that provides an interface to programmically interact with dbt projects. """
 
 
