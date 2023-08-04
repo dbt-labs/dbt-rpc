f234e1b09bee4b265998d6293a0f21d9ea0955a6
Gerda Shank <gerda@dbtlabs.com>
2023-08-04 14:52:58 -0400

    Bump version to 0.3.4 (#151)

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 
 package_name = "dbt-rpc"
-package_version = "0.3.3"
+package_version = "0.3.4"
 description = """ A JSON RPC server that provides an interface to programmically interact with dbt projects. """
 
 
